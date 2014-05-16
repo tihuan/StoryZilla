7acfced6e0ee294e07e9a647d2621b1c988d7f51
Jared Rader <raderj89@gmail.com>
2014-05-16 08:42:45 -0400

    shared error messages

--- storyzilla/spec/factories.rb
+++ storyzilla/spec/factories.rb
@@ -1,7 +1,7 @@
 FactoryGirl.define do
   factory :user do
-    username "example_user"
-    email "user@example.com"
+    sequence(:username) { |n| "user-#{n}" }
+    sequence(:email) { |n| "user-#{n}@example.com" }
     password "password"
     password_confirmation "password"
   end
